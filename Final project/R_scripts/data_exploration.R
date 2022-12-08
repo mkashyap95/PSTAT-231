aca54d9f2f69b6886cdd6cce04ab479a25acf9f2
Madhuri Kashyap <m_kashyap@ucsb.edu>
2022-12-08 15:36:53 +0530

    processing complete

--- Final project/R_scripts/data_exploration.R
+++ Final project/R_scripts/data_exploration.R
@@ -3,12 +3,26 @@
 #packages
 library(dplyr)
 library(tidyselect)
+library(corrplot)
+library(ggplot2)
+library(GGally)
+library(caret)
 
 #reading in data
 setwd("C:\\Users\\kashyap\\Desktop\\PSTAT-231\\Final project\\data\\processed")
 df_comp <- read.csv("cleaned.csv")
 
-
-#pair plot
+#heatmap
 df_numeric <- select_if(df_comp, is.numeric)
-pairs(df_numeric)
+df_numeric %>%
+  cor() %>%
+  corrplot(type = "lower", diag = F, method = "color")
+
+#dropping owned users
+df_comp <- subset(df_comp, select=-owned_users)
+
+
+###OHE
+
+#dummy variables
+
--- Final project/R_scripts/data_processing.R
+++ Final project/R_scripts/data_processing.R
@@ -4,6 +4,8 @@
 library(janitor)
 library(tidyverse)
 library(tidyselect)
+library(keras)
+library(dplyr)
 
 
 #reading in data
@@ -12,8 +14,10 @@ df <- read.csv("bgg_dataset.csv", sep = ";")
 df_clean <- as_tibble(df) %>%
   clean_names()
 
+
 #data overview
 summary(df_clean)
+
 
 #changing rating scores to numbers
 convert_string <- function(x){
@@ -26,7 +30,9 @@ summary(df_clean)
 
 
 #dropping the NA values - mostly in Owned users
-df_comp <- na.omit(df_clean)
+df_comp <- drop_na(df_clean)
+summary(df_comp)
+
 
 #changing year published to a time scale by converting it to game age
 convert_year <- function(y){
@@ -34,12 +40,15 @@ convert_year <- function(y){
 }
 df_comp$game_age <- sapply(df_comp$year_published, convert_year)
 
+
 #summary of new df
 summary(df_comp)
+
 
 #checking nas in domain and mechanics
 df_comp[19000,"domains"]
 df_comp[df_comp == ""] <- NA
+
 
 #counting NAs
 dim(df_comp[is.na(df_comp$domains),])
@@ -53,6 +62,7 @@ dim(df_comp[!is.na(df_comp$mechanics),])
 df_comp$domains <- replace(df_comp$domains, is.na(df_comp$domains), "Basic")
 df_comp$mechanics <- replace(df_comp$mechanics, is.na(df_comp$mechanics), "Not mentioned")
 
+
 #some special characters in mechanics -- change to commas
 clean_string <- function(z){
   str_replace_all(z, "/",",")
@@ -62,10 +72,24 @@ df_comp$mechanics <- lapply(df_comp$mechanics, clean_string)
 df_comp$domains
 df_comp$mechanics
 
+
 #converting to string
 df_comp$mechanics <- as.character(df_comp$mechanics)
+df_comp$mechanics[10]
+
+df_comp <- as.data.frame(df_comp)
+
+#dropping unused variables
+df_comp <- subset(df_comp, select=-c(id,year_published))
+
+#adding stars -- binned column for rating
+df_comp$Stars = cut(df_comp$rating_average, breaks = seq(0,10,2), labels = c(1,2,3,4,5))
+
+
+#tokenizing mechanics and domains
+# tokenizer_mech <- text_tokenizer(split = ",") %>%
+#   fit_text_tokenizer(df_comp$domains)
+
 
 #saving to csv
 write.csv(df_comp, "C:\\Users\\kashyap\\Desktop\\PSTAT-231\\Final project\\data\\processed\\cleaned.csv", row.names=FALSE)
-
-
